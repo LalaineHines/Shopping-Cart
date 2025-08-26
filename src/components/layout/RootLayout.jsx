6246aa42563c0fbe58423342935599078ee7b86a
LalaineHines <lalainehines@gmail.com>
2025-08-25 19:01:10 -0700

    added script for root layout

---
+++ src/components/layout/RootLayout.jsx
@@ -0,0 +1,49 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
+
+const RootLayout = () => {
+  const [cart, setCart] = useState(() => {
+    const saved = localStorage.getItem("cart");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  const [products, setProducts] = useState(() => {
+    const saved = localStorage.getItem("products");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  const [savings, setSavings] = useState(() => {
+    const saved = localStorage.getItem("savings");
+    return saved ? JSON.parse(saved) : 0;
+  });
+
+  const [totalPriceBeforeTax, setTotalPriceBeforeTax] = useState(() => {
+    const saved = localStorage.getItem("totalPriceBeforeTax");
+    return saved ? JSON.parse(saved) : 0;
+  });
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header cart={cart} />
+      <main className="flex-grow">
+        <Outlet
+          context={{
+            cart,
+            setCart,
+            products,
+            setProducts,
+            savings,
+            setSavings,
+            totalPriceBeforeTax,
+            setTotalPriceBeforeTax,
+          }}
+        />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default RootLayout;
