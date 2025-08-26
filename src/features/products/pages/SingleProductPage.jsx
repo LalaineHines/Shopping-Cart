bb60b051066d2baff38fc09e1a8a75999a3bef4b
LalaineHines <lalainehines@gmail.com>
2025-08-26 09:37:58 -0700

    added single product functionality

---
+++ src/features/products/pages/SingleProductPage.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+import SingleProductCard from "../components/SingleProductCard/SingleProductCard";
+import Spinner from "../../../components/Spinner";
+import { useParams } from "react-router-dom";
+
+const SingleProductPage = () => {
+    const { id } = useParams();
+
+    const [product, setProduct] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            try {
+                const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+                if (!res.ok) throw new Error("Failed to fetch product");
+                const data = await res.json();
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProduct();
+    }, [id]);
+
+    if (error) return <p>Error: {error}</p>;
+    if (loading)
+        return (
+    <div className="flex justify-center items-center h-screen">
+        <Spinner />
+    </div>
+    );
+
+    return (
+        <div> 
+            <SingleProductCard key={product.id} product={product} />
+        </div>
+    );
+};
+
+export default SingleProductPage;
