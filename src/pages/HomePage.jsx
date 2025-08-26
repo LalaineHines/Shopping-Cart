879b3b735398e532bcce4420bfe419f880317a0a
LalaineHines <lalainehines@gmail.com>
2025-08-25 19:29:06 -0700

    added home page script configs

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+  return (
+    <div className="flex items-center justify-center flex-col h-screen">
+      <h1 className="lg:text-6xl text-5xl text-center font-bold">
+        Shop quickly with{" "}
+        <span className="bg-gradient-to-r from-purple-400 to-[#101828] bg-clip-text text-transparent">
+          QuicKCart.
+        </span>
+      </h1>
+      <p className="text-zinc-400 mb-2 mt-10 lg:w-200 w-100 text-center">
+        Your one-stop shop for fast, easy, and reliable online shopping. Get
+        what you need in just a few clicks.
+      </p>
+      <div className="mt-4">
+        <Link
+          to="/products"
+          className="bg-purple-900 rounded-lg p-3 mr-3 cursor-pointer"
+        >
+          Start Shoping
+        </Link>
+        <Link
+          to="/about"
+          className="bg-[#102d46] rounded-lg p-3 cursor-pointer"
+        >
+          Learn more
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
