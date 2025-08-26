e4cea77c46a73ba05727d0e327b3e1c5bea0d3bf
LalaineHines <lalainehines@gmail.com>
2025-08-26 10:27:03 -0700

    added category func

---
+++ src/features/products/components/ProductFilters/CategoriesFilter.jsx
@@ -0,0 +1,37 @@
+const CategoriesFilter = ({ category, handleCategoryClick = () => {} }) => {
+  const categories = [
+    "All",
+    "Women's clothing",
+    "Men's clothing",
+    "Jewelry",
+    "Electronics",
+  ];
+  return (
+    <div className="w-[360px] h-60 bg-gray-800 p-4 rounded-xl shadow-lg transition-transform duration-300 font-sans overflow-hidden m-4">
+      <div className="flex items-center justify-between mb-4">
+        <h5 className="text-xl font-bold leading-none text-gray-900">
+          Categories:
+        </h5>
+      </div>
+      <div className="flow-root">
+        <ul className="flex flex-col items-center justify-center">
+          {categories.map((cat) => (
+            <li
+              key={cat.toLowerCase()}
+              onClick={() => handleCategoryClick(cat.toLowerCase())}
+              className={`cursor-pointer mb-2 ${
+                category === cat.toLowerCase()
+                  ? "font-bold text-blue-500"
+                  : "text-gray-600"
+              }`}
+            >
+              {cat}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default CategoriesFilter;
