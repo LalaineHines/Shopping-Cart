e617158cddd0c3301e0deff70505348d3582b464
LalaineHines <lalainehines@gmail.com>
2025-08-25 19:11:51 -0700

    added navbar funcs

---
+++ src/components/layout/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import styles from "./Navbar.modules.css";
+
+const Navbar = () => {
+    return (
+        <div className={styles.navDiv}>
+            <ul className={styles.navbar}>
+                <li className={styles.navItem}>
+                    <Link to="/home" className={`${styles.link} ${styles.active}`}>
+                    Home
+                    </Link>
+                </li>
+                <li className={styles.navItem}>
+                    <Link to="/products" className={`${styles.link} ${styles.active}`}>
+                    Products
+                    </Link>
+                </li>
+                <li className={styles.navItem}>
+                    <Link to="/about" className={`${styles.link} #{styles.active}`}>
+                    About
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;
